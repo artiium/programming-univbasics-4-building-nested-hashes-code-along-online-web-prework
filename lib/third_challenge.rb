52f2e5ebbb1446807c2e21bbbc2df138b62b7093
Artiium Arbyummi <artiium21@gmail.com>
2019-09-06 15:49:29 +0000

    Automatically backed up by Learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -14,7 +14,7 @@ def third_challenge
         age: "15",
         status: "alive"
         },
-      :hero_friends => [{name: "Benvolio", age: "17", attitude: "worried"}
+      :hero_friends => [{name: "Benvolio", age: "17", attitude: "worried"},
       {name: "Mercutio", age: "18", attitude: "hot-headed"}]
    }, 
    :capulet => {
@@ -31,7 +31,7 @@ def third_challenge
         age: "15",
         status: "alive"
         },
-      :heroine_friends => [{name: "Steven", age: "30", attitude: "confused"}
+      :heroine_friends => [{name: "Steven", age: "30", attitude: "confused"},
       {name: "Nurse", age: "44", attitude: "worried"}]
    }
   }
